d7cba8c2062b4fceb0f159e542d85d420eab604a
john horo <johnhoro125@gmail.com>
2021-12-14 12:56:51 +0530

    cls

---
+++ block-BJaaik/code/index.js
@@ -0,0 +1,61 @@
+class Person {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  eat() {
+    return `Eats well cooked food`;
+  }
+  sleep() {
+    return `Sleep 8 hours a day`;
+  }
+  walk() {
+    return `Walks long to remain healthy`;
+  }
+}
+
+class Player extends Person {
+  constructor(sportsName, name, age, gender) {
+    super(name, age, gender);
+    this.sportsName = sportsName;
+  }
+  plays() {
+    return `Plays ${this.sportsName} very well`;
+  }
+}
+
+class Cricketer extends Player {
+  constructor(teamName, sportsName, name, age, gender) {
+    super(sportsName, name, age, gender);
+    this.teamName = teamName;
+  }
+  playCricket() {
+    return `Plays Cricket as an allRounder`;
+  }
+}
+
+class Teacher extends Person {
+  constructor(instituteName, name, age, gender) {
+    super(name, age, gender);
+    this.instituteName = instituteName;
+  }
+  teach() {
+    return `Teaches Well`;
+  }
+}
+
+class Artist extends Person {
+  constructor(kind, name, age, gender) {
+    super(name, age, gender);
+    this.kind = kind;
+  }
+  createArt() {
+    return `Is expert in ${this.kind} art`;
+  }
+}
+
+let artist1 = new Artist(`Singing`, `Joy`, 25, `Male`);
+let teacher1 = new Teacher(`AltCampus`, `Ankit`, 27, `Male`);
+let crickter1 = new Cricketer(`India`, `Cricket`, `Virat Kohli`);
+let player1 = new Player(`Badminton`, `Sania Nehwal`, 28, `female`);
